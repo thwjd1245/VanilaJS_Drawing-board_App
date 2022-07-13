0a03c84f33dd45a259c9764b003e39a1a0cb0e8c
thwjd1245 <thwjd01245@naver.com>
2022-07-13 19:01:54 +0900

    canvas practice

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-a
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d"); //그림 그릴때 사용 (context)
+
+canvas.width = 800;
+canvas.height = 800;
+
+
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.stroke();
